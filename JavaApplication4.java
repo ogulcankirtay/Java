272c4f55fe69c501d6dad5775438122386f6247d
ogulcankirtay <56118583+ogulcankirtay@users.noreply.github.com>
2020-10-11 18:30:41 +0300

    Add files via upload

---
+++ JavaApplication4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JavaApplication4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a[]=new int[5];
+        a[4]=7;
+        System.out.println(a);
+        System.out.println(a[0]);
+        System.out.println(a[4]);
+        int b[]={1,2,3,4};
+        System.out.println(b[1]);
+        String str[]={"merhaba","a","nasısın","iyiyim"};
+         System.out.println(str[str.length-1]);
+
+    
+    }
+    
+}
