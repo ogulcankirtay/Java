386e96160fd935cccc0699f93f260111b766a7cd
ogulcankirtay <56118583+ogulcankirtay@users.noreply.github.com>
2021-01-20 18:30:44 +0300

    Add files via upload

--- a190202005.java
+++ a190202005.java
@@ -1,21 +1,32 @@
-package calısma1;
-// Oğulcan Kırtay 190202005
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package calısma2;
+//Oğulcan Kırtay 190202005  
+
+import java.util.Scanner;
 
 /**
  *
  * @author ASUS
  */
 public class a190202005 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-           int result=1;
-        for (int i = 1; i <=10; i++) {
-          result*=i;
-        }
-        System.out.println("10!: "+result);
+        // TODO code application logic here
+        String a, b, c;
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("enter 3 words: ");
+        a = myobj.next();
+        b = myobj.next();
+        c = myobj.next();
+        System.out.println(a + "\\" + b + "\\" + c);
+
     }
+
 }
